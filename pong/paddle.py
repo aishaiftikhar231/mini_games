8ca89ecc23aae3400eee7116b895d846bab18c83
aishaiftikhar231 <aishathewitch231@gmail.com>
2024-04-20 21:03:34 +0500

    test

---
+++ pong/paddle.py
@@ -0,0 +1,21 @@
+import turtle
+from turtle import Screen
+
+# Define the Paddle class if not already defined in a separate module
+class Paddle(turtle.Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
